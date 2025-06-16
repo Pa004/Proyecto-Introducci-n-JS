813d069249f96d14c53f458e852704c27f2978b8
Pablo <padominguez@espe.edu.ec>
2025-06-16 12:56:14 -0500

    Operaciones basicas

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,36 @@
-function saludar() {
-    let saludo = document.getElementById("mostrar_saludo");
-    saludo.textContent = "¡Hola mundo usando textContent!";
-    console.log(saludo);
+function calcular_Suma() {
+    let n1 = document.getElementById("psuma").value;
+    let n2 = document.getElementById("ssuma").value;
+
+    let res = parseFloat(n1) + parseFloat(n2);
+    console.log("Resultado de la suma: " + res);
+}
+
+function calcular_Resta() {
+    let n1 = document.getElementById("presta").value;
+    let n2 = document.getElementById("sresta").value;
+
+    let res = parseFloat(n1) - parseFloat(n2);
+    console.log("Resultado de la resta: " + res);
+}
+
+function calcular_Multiplicacion() {
+    let n1 = document.getElementById("pmul").value;
+    let n2 = document.getElementById("smul").value;
+
+    let res = parseFloat(n1) * parseFloat(n2);
+    console.log("Resultado de la multiplicación: " + res);
+}
+
+function calcular_Division() {
+    let n1 = document.getElementById("pdiv").value;
+    let n2 = document.getElementById("sdiv").value;
+
+    if (parseFloat(n2) == 0) {
+        console.log("Error: División por cero no permitida.");
+        return;
+    }
+
+    let res = parseFloat(n1) / parseFloat(n2);
+    console.log("Resultado de la división: " + res);
 }
